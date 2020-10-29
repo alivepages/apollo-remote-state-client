f32d5325d1d673809f56126a311570ed6a0e964d
Joel Fagundo <joel.fagundo@gmail.com>
2020-10-29 11:40:57 -0600

    public

---
+++ apollo.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  client: {
+    service: {
+      name: 'ac3-todos-backend',
+      url: `https://apollo-remote-state-server.herokuapp.com//'
+    },
+  },
+};
---
+++ src/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { render } from 'react-dom'
+import App from './components/App'
+import { ApolloClient, ApolloProvider } from '@apollo/client';
+import 'todomvc-app-css/index.css'
+import { cache } from './cache';
+
+
+const PORT = process.env.PORT || 400;
+
+export const client = new ApolloClient({
+  cache,
+  uri: 'https://apollo-remote-state-server.herokuapp.com/',
+  headers: {
+    authorization: localStorage.getItem('token') || '',
+    'client-name': 'ac3-todos-backend',
+    'client-version': '1.0.0',
+  },
+  connectToDevTools: true,
+});
+
+render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+)
